7752df0d48d3d3ddf1d85542a726e1e512354577
dmarta14 <diegomarta14@gmail.com>
2023-05-16 13:51:35 +0200

    Primera progración FormulariosDatos

--- app/src/main/java/com/example/dasmeet/FormularioDatos.java
+++ app/src/main/java/com/example/dasmeet/FormularioDatos.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FormularioDatos  extends AppCompatActivity {
 
-
+    RadioGroup sexo,pelo,ojos;
+    int selectSexo,selectPelo,selectOjos;
+    CheckBox personalidad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,45 @@ public class FormularioDatos  extends AppCompatActivity {
         siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sexo = findViewById(R.id.radio_group_respuestas1);
+                selectSexo = sexo.getCheckedRadioButtonId();
+
+                if(selectSexo != -1){
+                    RadioButton sexoSelecionado= findViewById(selectSexo);
+                    String respuestaSelecionada = sexoSelecionado.getText().toString();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Debes seleccionar un sexo en la pregunta 1", Toast.LENGTH_LONG).show();
+                }
+                ojos = findViewById(R.id.radio_group_respuestas2);
+                selectOjos = ojos.getCheckedRadioButtonId();
+
+                if(selectOjos != -1){
+                    RadioButton ojosSelecionado= findViewById(selectOjos);
+                    String respuestaSelecionada = ojosSelecionado.getText().toString();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Debes seleccionar un color de ojos en la pregunta 2", Toast.LENGTH_LONG).show();
+                }
+
+                pelo = findViewById(R.id.radio_group_respuestas2);
+                selectPelo = pelo.getCheckedRadioButtonId();
+
+                if(selectPelo != -1){
+                    RadioButton peloSelecionado= findViewById(selectPelo);
+                    String respuestaSelecionada = peloSelecionado.getText().toString();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Debes seleccionar un color de pelo en la pregunta 3", Toast.LENGTH_LONG).show();
+                }
+
+
+
+                Intent intent = new Intent(v.getContext(),FormularioGustos.class);
+                startActivity(intent);
             }
         });
     }
